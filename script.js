4e35a0e0c160657a15d31e7404773d3307449956
amnaajmal <38943088+amnaajmal@users.noreply.github.com>
2018-07-19 23:13:54 +0500

    project

---
+++ script.js
@@ -0,0 +1,5 @@
+function calage(){
+	let date = document.getElementByClass("input1").value;
+	let month = document.getElementByClass("input2").value;
+	let year = document.getElementByClass("input3").value;
+}
