fd6db99ce094c6e92448d96114ada89a1f7688a8
amnaajmal <38943088+amnaajmal@users.noreply.github.com>
2018-07-30 14:16:32 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function calage(){
-	let date = document.getElementByClass("input1").value;
-	let month = document.getElementByClass("input2").value;
-	let year = document.getElementByClass("input3").value;
-}
-function
